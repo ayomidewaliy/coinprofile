d8dfe26e88e80c32756e22297d0c8ea7eca6c612
Waliyu Raji <mwaliyayomide@gmail.com>
2022-11-02 15:12:58 +0100

    chore: install packages

--- android/app/src/main/java/com/coinprofile/MainActivity.java
+++ android/app/src/main/java/com/coinprofile/MainActivity.java
@@ -1,5 +1,7 @@
 package com.coinprofile;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -14,6 +16,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Coinprofile";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
